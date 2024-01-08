46026548c89682675e90eaf62248f5cceb597a66
Rissshabh <kushwaharishabh21@gmail.com>
2024-01-08 17:18:53 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123;
+let accountEmail = "Kushwaharishabh21@gmail.com"
+var accountPassword = "adf3a2fd32"
+city = "Kanpur"
+let accountState;
+
+
+// accountId = 12 assignment not allowed
+accountEmail = "Kushwaha@gmail.com"
+accountPassword = "afd1111111"
+city = "Delhi"
+/*
+Prefer not to use the var because of the scope issues and the functional scopes
+*/
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword, city, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello") we are using the node.js not the browser
+
+console.log(3+3)
+console.log("Rishabh Kushwaha")
+
+// Data Types 
+let name = "Kushwaha"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string = "" 
+// boolean = true / false 
+// null => it is a standalone value 
+// undefined =>
+// symbol => whenever we talk about unique
+
+// object 
+
+// console.log(typeof age)
+// console.log(typeof null) => type is object
+// console.log(typeof undefined) => type is undefined
