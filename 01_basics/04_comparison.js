c0adf14e36fec3ad59696a54b4a8bcad906eca31
Rissshabh <kushwaharishabh21@gmail.com>
2024-01-09 18:50:19 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1)
+// console.log("0" > 0)
+
+// avoid this type of conversion
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// avoid this type of conversion 
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined >= 0)
+
+// ===
+console.log("2" == 2)
+console.log("2" === 2)
