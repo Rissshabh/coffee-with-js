83100a684c6c9e85b9f973706152bf3051c37971
Rissshabh <kushwaharishabh21@gmail.com>
2024-01-09 18:41:59 +0000

    updated conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,19 +11,53 @@
 // "33abc" => NaN
 // true => 1 : false => 0
 
-let isLoggedIn = "Rishabh"
+// let isLoggedIn = "Rishabh"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true : 0 => false
 // "" => false
 // "Rishabh" => false
 
-let someNum = 33
+// let someNum = 33
 
-let strNum = String(someNum)
+// let strNum = String(someNum)
 
-console.log(strNum)
-console.log(typeof strNum)
+// console.log(strNum)
+// console.log(typeof strNum)
+
+// **********************   Operations     **************************
+
+let value = 3
+let negVal = -value
+// console.log(negVal)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+// let str1 = "Rishabh"
+// let str2 = " Kushwaha"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
