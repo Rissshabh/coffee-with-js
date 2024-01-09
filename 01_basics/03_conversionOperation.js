30c7894e3ac7f2f689b6fbbef99cf32a02735dc4
Rissshabh <kushwaharishabh21@gmail.com>
2024-01-09 18:28:10 +0000

    coonversion updated part 2

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,29 @@
-let score = "33abc"
+// let score = "Rishabh"
 
-console.log(typeof score)
+// console.log(typeof score)
 
-let valueInNum = Number(score)
-console.log(typeof valueInNum)
-console.log(valueInNum)
+// let valueInNum = Number(score)
+// console.log(typeof valueInNum)
+// console.log(valueInNum)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 : false => 0
+
+let isLoggedIn = "Rishabh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 => true : 0 => false
+// "" => false
+// "Rishabh" => false
+
+let someNum = 33
+
+let strNum = String(someNum)
+
+console.log(strNum)
+console.log(typeof strNum)
