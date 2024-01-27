3ec3d340c8c556a0ee8b72d6e9122c32dee3cc5f
Rissshabh <kushwaharishabh21@gmail.com>
2024-01-27 16:24:21 +0000

    reduce method

---
+++ 05_iterations/nine.js
@@ -0,0 +1,32 @@
+const myNums = [1, 2, 3, 4, 5]
+
+// const myTotal = myNums.reduce(function (acc, currVal) {
+//     console.log(`acc: ${acc} and currVal: ${currVal}`)
+//     return acc + currVal
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, currVal) => acc + currVal, 0)
+
+console.log(myTotal)
+
+const shoppingCart = [
+    {
+        itemName : "js course",
+        price : 2999
+    },
+    {
+        itemName : "py course",
+        price : 999
+    },
+    {
+        itemName : "mobile dev course",
+        price : 5999
+    },
+    {
+        itemName : "data engineer course",
+        price : 12999
+    },
+]
+
+const total = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+console.log(total)
